debd76bfc19a3898ee729f5e365af8018eb7c960
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2015-11-08 15:20:14 -0800

    replace state space explorer with A* search

--- ai/src/com/teamquixote/ai/StateSpaceExplorer.java
+++ ai/src/com/teamquixote/ai/StateSpaceExplorer.java
@@ -1,7 +1,8 @@
 package com.teamquixote.ai;
 
-        import java.util.*;
-        import java.util.stream.Collectors;
+import com.teamquixote.ai.actions.Action;
+
+import java.util.*;
 
 public class StateSpaceExplorer {
     private final GameStateUtility utilityFunction;
@@ -10,39 +11,63 @@ public class StateSpaceExplorer {
         this.utilityFunction = utilityFunction;
     }
 
-    public GameState findBestState(GameState state) {
-        Queue<StateUtilityPair> states = new PriorityQueue<>();
-        Set<GameState> exploredStates = new HashSet<>();
-        states.addAll(state.getActions().stream()
-                .map(a -> new StateUtilityPair(new GameState(state, a)))
-                .collect(Collectors.toList()));
+    public GameState findBestState(GameState start) {
+        Set<GameState> explored = new HashSet<>();
+        Queue<StateCostPair> open = new PriorityQueue<>();
+        Map<GameState, Double> costs = new HashMap<>();
 
-        GameState current = null;
-        while (!states.isEmpty() && !utilityFunction.isTerminalState((current = states.remove().state))) {
-            exploredStates.add(current);
-            final GameState finalCurrent = current;
-            for (GameState sPrime : current.getActions().stream().map(a -> new GameState(finalCurrent, a)).collect(Collectors.toList()))
-                if (!exploredStates.contains(sPrime)) states.add(new StateUtilityPair(sPrime));
+        open.add(new StateCostPair(start));
+        costs.put(start, 0.0);
+
+        while (!open.isEmpty()) {
+            StateCostPair p = open.remove();
+            GameState current = p.state;
+
+            if (current.previousAction != null && utilityFunction.isTerminalState(current)) {
+                return current;
+            }
+
+            if (!explored.contains(current))
+                explored.add(current);
+            for (Action a : current.getActions()) {
+                StateCostPair fringe = new StateCostPair(new GameState(current, a));
+
+                double edgeCost = a.getActionCost(current);
+                double tentativeCost = costs.get(current) + edgeCost;
+                double currentCost = costs.getOrDefault(fringe.state, Double.POSITIVE_INFINITY);
+
+                //if the cost of this path is lower than the current path (or no current path exists), add it to the
+                // queue of fringe states
+                if (tentativeCost < currentCost) {
+                    costs.put(fringe.state, tentativeCost);
+                    //if this state is already in our priority queue, ideally we'd just update the priority.
+                    // However, this is only possible by removing and adding the item again, which is a O(n)
+                    // operation.  Instead, we'll just add a duplicate item and skip exploring it if it comes up
+                    // again
+                    open.add(fringe);
+                }
+            }
         }
 
-        return current;
+        //no goal state found
+        return null;
     }
 
-    private class StateUtilityPair implements Comparable<StateUtilityPair> {
+    private class StateCostPair implements Comparable<StateCostPair> {
         public final GameState state;
         public final double utility;
 
-        public StateUtilityPair(GameState state) {
+        public StateCostPair(GameState state) {
             this(state, utilityFunction.getUtility(state));
         }
 
-        public StateUtilityPair(GameState state, double utility) {
+        public StateCostPair(GameState state, double utility) {
             this.state = state;
             this.utility = utility;
         }
 
         @Override
-        public int compareTo(StateUtilityPair o) {
+        public int compareTo(StateCostPair o) {
             if(utility > o.utility) return 1;
             if(utility < o.utility) return -1;
             return 0;
--- ai/src/com/teamquixote/ai/actions/Action.java
+++ ai/src/com/teamquixote/ai/actions/Action.java
@@ -5,6 +5,10 @@ import com.teamquixote.ai.GameState;
 public abstract class Action {
     public abstract void execute();
 
+    public double getActionCost(GameState state){
+        return 1.0;
+    }
+
     public int getUpdatedHeroPosition(GameState state) {
         //implementing classes can override this if their actions cause a change in hero location
         return state.heroPosition;
