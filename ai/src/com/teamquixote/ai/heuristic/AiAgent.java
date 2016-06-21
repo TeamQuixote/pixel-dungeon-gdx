9eea6cf077c249172e6abe0bf8414133812347b4
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2016-06-21 07:44:10 -0700

    fix merge fuckup

--- ai/src/com/teamquixote/ai/heuristic/AiAgent.java
+++
@@ -1,7 +0,0 @@
-package com.teamquixote.ai.heuristic;
-
-import com.teamquixote.ai.heuristic.actions.Action;
-
-public abstract class AiAgent {
-    protected abstract Action makeDecision(GameState state);
-}
